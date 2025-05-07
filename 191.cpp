5765429ab8abcb016c3e0e0b40b9d2c9a756d26e
Faiza <afiffaiza5@gmail.com>
2025-05-07 17:51:03 +0530

    completed problem 191

---
+++ 191.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/number-of-1-bits/description/
+
+class Solution {
+    public:
+        int hammingWeight(int n) {
+            int count=0;
+            string s="";
+            while(n !=0 ){
+                if( n%2 == 0) s+= '0';
+                else s+= '1';
+                n/=2;
+            }
+            int len = s.length();
+            for(int i=0; i<len; i++){
+                if(s[i] == '1') count++;
+            }
+            return count;
+        }
+    };
