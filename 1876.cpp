13d0a5b1460685f0cb174f7097628c641af37f2f
Faiza <afiffaiza5@gmail.com>
2025-07-24 21:26:49 +0530

    modified

--- 1876.cpp
+++ 1876.cpp
@@ -5,7 +5,6 @@ public:
     int countGoodSubstrings(string s) {
         int n = s.length();
         int count = 0;
-        string ans = "";
         for(int i = 1; i < n-1; i++){
             if((s[i] != s[i+1]) and (s[i] != s[i-1]) and (s[i-1] != s[i+1])) count++;
         }
