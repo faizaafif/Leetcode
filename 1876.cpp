7f13572484229a9210f584b99e811e517d166bb2
Faiza <afiffaiza5@gmail.com>
2025-07-24 21:14:26 +0530

    completed problem 1876

---
+++ 1876.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/substrings-of-size-three-with-distinct-characters/description/
+
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+        int n = s.length();
+        int count = 0;
+        string ans = "";
+        for(int i = 1; i < n-1; i++){
+            if((s[i] != s[i+1]) and (s[i] != s[i-1]) and (s[i-1] != s[i+1])) count++;
+        }
+        return count;
+    }
+};
