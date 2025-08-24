ac3be7ded675841ad793796e366d6340c123c928
Faiza <afiffaiza5@gmail.com>
2025-08-24 11:51:51 +0530

    completed problem 643

---
+++ 643.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/maximum-average-subarray-i/description/
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
+        double sum = 0;
+        for(int i = 0; i < k; i++){
+            sum += nums[i];
+        }
+        double max = sum;
+        for(int i = k; i < n; i++){
+            sum += nums[i] - nums[i-k];
+            if(sum > max) max = sum;
+        }
+        return max/k;
+    }
+};
