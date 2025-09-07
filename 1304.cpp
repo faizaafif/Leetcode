4d61494522e8e78ec148b86611d2d3e9d27daeb7
Faiza <afiffaiza5@gmail.com>
2025-09-07 20:47:18 +0530

    completed problem 1304

---
+++ 1304.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/description/
+
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> arr;
+        for(int i = 1; i <= n/2; i++){
+            arr.push_back(i);
+            arr.push_back(-i);
+        }
+        if(n % 2 != 0) arr.push_back(0);
+        return arr;
+    }
+};
