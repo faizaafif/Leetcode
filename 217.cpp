6cfa3fede6266ea13467ca79f45a3f172a4f20c0
Faiza <afiffaiza5@gmail.com>
2025-11-21 12:22:54 +0530

    completed problem 217

---
+++ 217.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/contains-duplicate/description/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int c = 0;
+        for(int i = 0; i < n-1; i++){
+            if(nums[i] == nums[i+1]) c++;
+        }
+        return c > 0;
+    }
+};
