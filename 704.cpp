c5b48d7a9f5f8df6b42f43ee03881b71ae7a8ecd
Faiza <afiffaiza5@gmail.com>
2026-02-11 17:45:11 +0530

    completed problem 704

---
+++ 704.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/binary-search/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0, h = n - 1;
+        while(l <= h){
+            int mid = l + (h - l) / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) h = mid - 1;
+            else l = mid + 1;
+        }
+        return -1;
+    }
+};
